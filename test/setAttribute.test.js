4258fdafe5b91b995847c1e2019c74fed75e4b40
damian <damian.morales@fizzmod.com>
2023-09-26 10:20:29 -0300

    arreglar manejo de excepxciones en app-crashlytics - primer arreglo

--- test/setAttribute.test.js
+++ test/setAttribute.test.js
@@ -5,5 +5,17 @@ describe('setAttribute function', () => {
     it('when receive strings of key and value', async () => {
       await setAttribute('name', 'value');
     });
+
+    it('when receive array of value it is parsed', async () => {
+      await setAttribute('key', ['value']);
+    });
+
+    it('when receive string of key null or undefined returns null', async () => {
+      expect(await setAttribute(undefined, 'value')).toBe(null);
+    });
+
+    it('when receive string of value null or undefined returns null', async () => {
+      expect(await setAttribute('key', null)).toBe(null);
+    });
   });
 });
--- test/setAttributes.test.js
+++ test/setAttributes.test.js
@@ -5,5 +5,13 @@ describe('setAttributed function', () => {
     it('when receive object of key and value', async () => {
       await setAttributes({name: 'value'});
     });
+
+    it('when receiving no string values as parameters', async () => {
+      await setAttributes({isMale: false, age: 15, admin: 'no'});
+    });
+
+    it('when receiving undefined or null are filtered', async () => {
+      await setAttributes({isMale: null, age: 15, admin: undefined});
+    });
   });
 });
